059991db6620baffc1eebe05ac9ff9309ef34c1b
Natalya <natalyamyunster@yahoo.com>
2019-08-03 07:11:47 +0200

    fixed syntax

--- Arrays.js
+++ Arrays.js
@@ -1,11 +1,10 @@
 //1. Reverse List Order 8 level
-````
-https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
-```
-// My solution:
+* https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b
 
+```javascript
 const reverseList = list =>  list.reverse();
-````
+```
+
 //2. Calculate average 8 level
 ````
 https://www.codewars.com/kata/calculate-average/javascript

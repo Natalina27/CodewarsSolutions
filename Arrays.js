d4dfa4d0f70e1853e9fc8897ed0c7b6fa77e2fb3
Natalya <natalyamyunster@yahoo.com>
2019-08-12 11:37:34 +0200

    add  solution Remove duplicate words 7level

--- Arrays.js
+++ Arrays.js
@@ -250,6 +250,16 @@ function getAverage(marks){
 const getAverage =(marks) => Math.floor(marks.reduce((sum, x) => sum + x) / marks.length);
 ```
 
+//25. Remove duplicate words 7level
+* https://www.codewars.com/kata/5b39e3772ae7545f650000fc
+```
+function removeDuplicateWords (s) {
+    return [...new Set(s.split(' '))].join(' ');
+}
+```
+
+
+
 
 
 

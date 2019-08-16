621b7bb8f2f081e4473695a67380c5f4791c326a
Natalya <natalyamyunster@yahoo.com>
2019-08-16 07:02:35 +0200

    add  solution How good are you really? 8level

--- Arrays.js
+++ Arrays.js
@@ -257,6 +257,16 @@ function removeDuplicateWords (s) {
     return [...new Set(s.split(' '))].join(' ');
 }
 ```
+//26. How good are you really? 8level
+* https://www.codewars.com/kata/5601409514fc93442500010b
+```
+function betterThanAverage(classPoints, yourPoints) {
+  let result=(classPoints.reduce((a,b)=>a+b,0)+yourPoints)/(classPoints.length+1);
+  return result < yourPoints;
+}
+```
+
+
 
 
 

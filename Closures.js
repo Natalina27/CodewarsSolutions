af8f133031ca97de9838c1f83502b936166ae4e0
Natalya <natalyamyunster@yahoo.com>
2019-08-19 08:58:26 +0200

    added Understanding closures - the basics 7level

--- Closures.js
+++ Closures.js
@@ -6,3 +6,7 @@ const calculate=(...x)=>(...y)=> [].concat(x,y).reduce((a,b)=>a+b,0);
 //const calculate = (...args1) => (...args2) => [...args1, ...args2].reduce((s, v) => s + v);
 ````
 
+//Understanding closures - the basics 7level
+* https://www.codewars.com/kata/56b71b1dbd06e6d88100092a
+````javascript
+````

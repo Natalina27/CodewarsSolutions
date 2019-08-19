69ff67035a6093c26562103124e270e4c48853e1
Natalya <natalyamyunster@yahoo.com>
2019-08-19 08:54:37 +0200

    added solution Javascript Mathematician 7level

---
+++ Closures.js
@@ -0,0 +1,8 @@
+//Javascript Mathematician 7level
+
+* https://www.codewars.com/kata/javascript-mathematician/javascript
+````javascript
+const calculate=(...x)=>(...y)=> [].concat(x,y).reduce((a,b)=>a+b,0);
+//const calculate = (...args1) => (...args2) => [...args1, ...args2].reduce((s, v) => s + v);
+````
+
